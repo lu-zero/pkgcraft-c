f5060038dd142fadb767c96d5966aa32dd4706b0
Tim Harder <radhermit@gmail.com>
2021-12-27 00:48:51 -0700

    bash: add initial support for hasv

--- src/bash/has.rs
+++ src/bash/has.rs
@@ -27,3 +27,32 @@ pub unsafe extern "C" fn has(args: &*mut *mut c_char, args_len: c_int) -> c_int 
     let haystack = &args[1..];
     !haystack.contains(needle) as c_int
 }
+
+/// Returns 0 if the first argument is found in the list of subsequent arguments, 1 otherwise.
+///
+/// Returns -1 if an error occurred.
+///
+/// # Safety
+/// Behavior is undefined if args is not a pointer to a length args_len array of
+/// valid UTF-8 strings.
+#[no_mangle]
+pub unsafe extern "C" fn hasv(args: &*mut *mut c_char, args_len: c_int) -> c_int {
+    let args = unsafe { args_to_vec(args, args_len) };
+
+    let needle = match args.first() {
+        Some(s) => s,
+        None => {
+            let err = Error::new("requires 1 or more args");
+            update_last_error(err);
+            return -1;
+        }
+    };
+
+    let haystack = &args[1..];
+    if haystack.contains(needle) {
+        println!("{}", needle);
+        0
+    } else {
+        1
+    }
+}
