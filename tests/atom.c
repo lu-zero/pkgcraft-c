01e0a9fd364df780fa8aec6785d8dd723135ba1e
Tim Harder <radhermit@gmail.com>
2022-01-23 13:53:15 -0700

    tests: try to use more consistent whitespace

--- tests/atom.c
+++ tests/atom.c
@@ -6,50 +6,50 @@
 #include <pkgcraft.h>
 
 int main (int argc, char **argv) {
-    char *atom, *val;
-    Atom *a;
+	char *atom, *val;
+	Atom *a;
 
-    if (argc == 2) {
-	atom = argv[1];	
-    } else if (argc < 2) {
-	fprintf(stderr, "missing required atom arg\n");
-	exit(1);
-    }
+	if (argc == 2) {
+		atom = argv[1];
+	} else if (argc < 2) {
+		fprintf(stderr, "missing required atom arg\n");
+		exit(1);
+	}
 
-    a = pkgcraft_atom(atom, NULL);
+	a = pkgcraft_atom(atom, NULL);
 
-    assert(strcmp(a->category, getenv("category")) == 0);
-    assert(strcmp(a->package, getenv("package")) == 0);
+	assert(strcmp(a->category, getenv("category")) == 0);
+	assert(strcmp(a->package, getenv("package")) == 0);
 
-    val = getenv("version");
-    if (val != NULL) {
-	assert(strcmp(a->version, val) == 0);
-    } else {
-	assert(a->version == NULL);
-    }
+	val = getenv("version");
+	if (val != NULL) {
+		assert(strcmp(a->version, val) == 0);
+	} else {
+		assert(a->version == NULL);
+	}
 
-    val = getenv("slot");
-    if (val != NULL) {
-	assert(strcmp(a->slot, val) == 0);
-    } else {
-	assert(a->slot == NULL);
-    }
+	val = getenv("slot");
+	if (val != NULL) {
+		assert(strcmp(a->slot, val) == 0);
+	} else {
+		assert(a->slot == NULL);
+	}
 
-    val = getenv("subslot");
-    if (val != NULL) {
-	assert(strcmp(a->subslot, val) == 0);
-    } else {
-	assert(a->subslot == NULL);
-    }
+	val = getenv("subslot");
+	if (val != NULL) {
+		assert(strcmp(a->subslot, val) == 0);
+	} else {
+		assert(a->subslot == NULL);
+	}
 
-    val = getenv("repo");
-    if (val != NULL) {
-	assert(strcmp(a->repo, val) == 0);
-    } else {
-	assert(a->repo == NULL);
-    }
+	val = getenv("repo");
+	if (val != NULL) {
+		assert(strcmp(a->repo, val) == 0);
+	} else {
+		assert(a->repo == NULL);
+	}
 
-    pkgcraft_atom_free(a);
+	pkgcraft_atom_free(a);
 
-    return 0;
+	return 0;
 }
