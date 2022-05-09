2ded89135293fc887284976ecf47bb63be279316
Tim Harder <radhermit@gmail.com>
2022-05-08 23:33:48 -0600

    atom: add initial version support

--- src/atom.rs
+++ src/atom.rs
@@ -9,8 +9,9 @@ use crate::error::update_last_error;
 use crate::macros::unwrap_or_return;
 use crate::Error;
 
-// force an opaque type to be defined in pkgcraft.h
+// force opaque types to be defined in pkgcraft.h
 pub struct Atom;
+pub struct Version;
 
 /// Parse a string into an atom using a specific EAPI. Pass NULL for the eapi argument in
 /// order to parse using the latest EAPI with extensions (e.g. support for repo deps).
@@ -161,4 +162,58 @@ pub unsafe extern "C" fn pkgcraft_atom_cpv(atom: NonNull<atom::Atom>) -> *const 
 #[no_mangle]
 pub unsafe extern "C" fn pkgcraft_atom_free(atom: NonNull<atom::Atom>) {
     let _ = unsafe { Box::from_raw(atom.as_ptr()) };
+}
+
+/// Parse a string into a version.
+///
+/// Returns NULL on error.
+///
+/// # Safety
+/// The version argument should point to a valid string.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_version(version: *const c_char) -> *mut atom::Version {
+    let ver_str = unsafe { unwrap_or_return!(CStr::from_ptr(version).to_str(), ptr::null_mut()) };
+    let ver = unwrap_or_return!(atom::parse::version(ver_str), ptr::null_mut());
+    Box::into_raw(Box::new(ver))
+}
+
+/// Compare two versions returning -1, 0, or 1 if the first version is less than, equal to, or greater
+/// than the second version, respectively.
+///
+/// # Safety
+/// The version arguments should be non-null Version pointers received from pkgcraft_version().
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_version_cmp(
+    v1: NonNull<atom::Version>,
+    v2: NonNull<atom::Version>,
+) -> c_int {
+    let (v1, v2) = unsafe { (v1.as_ref(), v2.as_ref()) };
+
+    match v1.cmp(v2) {
+        Ordering::Less => -1,
+        Ordering::Equal => 0,
+        Ordering::Greater => 1,
+    }
+}
+
+/// Return a given version's revision, e.g. the version "1-r2" has a revision of "2".
+///
+/// # Safety
+/// The version argument should be a non-null Version pointer received from pkgcraft_version().
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_version_revision(
+    version: NonNull<atom::Version>,
+) -> *const c_char {
+    let version = unsafe { version.as_ref() };
+    let s = version.revision().map(|r| r.as_str()).unwrap_or("");
+    CString::new(s).unwrap().into_raw()
+}
+
+/// Free a version.
+///
+/// # Safety
+/// The version argument should be a non-null Version pointer received from pkgcraft_version().
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_version_free(version: NonNull<atom::Version>) {
+    let _ = unsafe { Box::from_raw(version.as_ptr()) };
 }
