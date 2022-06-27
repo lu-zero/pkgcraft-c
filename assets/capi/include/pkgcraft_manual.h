9b091b564682e6b30d9e964af93e632bf7e642b0
Tim Harder <radhermit@gmail.com>
2022-06-26 18:35:05 -0600

    assets: add note about related cbindgen upstream issue
    [skip ci]

--- assets/capi/include/pkgcraft_manual.h
+++ assets/capi/include/pkgcraft_manual.h
@@ -1,4 +1,6 @@
-// Manually created header for items cbindgen can't handle properly.
+// Manually created header for items cbindgen can't handle properly, notably
+// fully qualified paths and `use` aliases when type names overlap.
+// See: https://github.com/eqrion/cbindgen/issues/7
 
 // Opaque wrapper for EbuildPkg objects.
 struct EbuildPkg;
