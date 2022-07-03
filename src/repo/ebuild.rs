f8095f0c9a7885d002223be099fc77e8b4c85609
Tim Harder <radhermit@gmail.com>
2022-07-02 18:32:12 -0600

    repo::ebuild: add initial support to get a repo's masters

--- src/repo/ebuild.rs
+++ src/repo/ebuild.rs
@@ -4,6 +4,7 @@ use std::os::raw::c_char;
 use std::ptr::NonNull;
 
 pub use pkgcraft::repo::ebuild::Repo as EbuildRepo;
+use pkgcraft::repo::Repo;
 
 /// Return a given ebuild repos's category dirs.
 ///
@@ -19,6 +20,28 @@ pub unsafe extern "C" fn pkgcraft_ebuild_repo_category_dirs(
         .category_dirs()
         .into_iter()
         .map(|s| CString::new(s).unwrap().into_raw())
+        .collect();
+    ptrs.shrink_to_fit();
+    unsafe { *len = ptrs.len() };
+    let ptr = ptrs.as_mut_ptr();
+    mem::forget(ptrs);
+    ptr
+}
+
+/// Return a given ebuild repos's masters.
+///
+/// # Safety
+/// The argument must be a non-null EbuildRepo pointer.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_ebuild_repo_masters(
+    r: NonNull<EbuildRepo>,
+    len: *mut usize,
+) -> *mut *mut Repo {
+    let repo = unsafe { r.as_ref() };
+    let mut ptrs: Vec<_> = repo
+        .masters()
+        .iter()
+        .map(|r| Box::into_raw(Box::new(Repo::Ebuild(r.clone()))))
         .collect();
     ptrs.shrink_to_fit();
     unsafe { *len = ptrs.len() };
