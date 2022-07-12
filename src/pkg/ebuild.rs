88a0c47665aa97db8b0b5a91dfce226bab83265b
Tim Harder <radhermit@gmail.com>
2022-07-11 19:46:57 -0600

    pkg::ebuild: add access to homepage, keywords, and iuse

--- src/pkg/ebuild.rs
+++ src/pkg/ebuild.rs
@@ -1,6 +1,6 @@
 use std::ffi::CString;
 use std::os::raw::c_char;
-use std::ptr;
+use std::{mem, ptr};
 
 pub use pkgcraft::pkg::ebuild::Pkg as EbuildPkg;
 
@@ -38,4 +38,70 @@ pub unsafe extern "C" fn pkgcraft_ebuild_pkg_description(p: *mut EbuildPkg) -> *
 pub unsafe extern "C" fn pkgcraft_ebuild_pkg_slot(p: *mut EbuildPkg) -> *mut c_char {
     let pkg = null_ptr_check!(p.as_ref());
     CString::new(pkg.slot()).unwrap().into_raw()
+}
+
+/// Return a package's homepage.
+///
+/// # Safety
+/// The argument must be a non-null EbuildPkg pointer.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_ebuild_pkg_homepage(
+    p: *mut EbuildPkg,
+    len: *mut usize,
+) -> *mut *mut c_char {
+    let pkg = null_ptr_check!(p.as_ref());
+    let mut ptrs: Vec<_> = pkg
+        .homepage()
+        .iter()
+        .map(|&s| CString::new(s).unwrap().into_raw())
+        .collect();
+    ptrs.shrink_to_fit();
+    unsafe { *len = ptrs.len() };
+    let ptr = ptrs.as_mut_ptr();
+    mem::forget(ptrs);
+    ptr
+}
+
+/// Return a package's keywords.
+///
+/// # Safety
+/// The argument must be a non-null EbuildPkg pointer.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_ebuild_pkg_keywords(
+    p: *mut EbuildPkg,
+    len: *mut usize,
+) -> *mut *mut c_char {
+    let pkg = null_ptr_check!(p.as_ref());
+    let mut ptrs: Vec<_> = pkg
+        .keywords()
+        .iter()
+        .map(|&s| CString::new(s).unwrap().into_raw())
+        .collect();
+    ptrs.shrink_to_fit();
+    unsafe { *len = ptrs.len() };
+    let ptr = ptrs.as_mut_ptr();
+    mem::forget(ptrs);
+    ptr
+}
+
+/// Return a package's iuse.
+///
+/// # Safety
+/// The argument must be a non-null EbuildPkg pointer.
+#[no_mangle]
+pub unsafe extern "C" fn pkgcraft_ebuild_pkg_iuse(
+    p: *mut EbuildPkg,
+    len: *mut usize,
+) -> *mut *mut c_char {
+    let pkg = null_ptr_check!(p.as_ref());
+    let mut ptrs: Vec<_> = pkg
+        .iuse()
+        .iter()
+        .map(|&s| CString::new(s).unwrap().into_raw())
+        .collect();
+    ptrs.shrink_to_fit();
+    unsafe { *len = ptrs.len() };
+    let ptr = ptrs.as_mut_ptr();
+    mem::forget(ptrs);
+    ptr
 }
